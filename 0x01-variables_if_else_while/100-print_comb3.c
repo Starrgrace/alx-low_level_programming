250685befb05b0c343451659c1f8489fc91bcdc0
Starrgrace <olasunkanmisekani17@gmail.com>
2022-09-10 15:58:24 -0700

    brian

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main - print single digit numbers starting from 0
  *
- * Return: return zero ar the end
+ *
+ * Description print
+ *
+ *
+ *
  */
-int main(void)
+int main()
 {
-	int i,j;
-
-	for (j = 1;j <=9;j++)
+	int n1 = 0, n2;
+	while (n <= 9)
 	{
-		if (j > i)
+		n2 = 0;
+		while(n2 <= 9)
 		{
-			putchar(i + '0');
-			putchar(j +'0');
 
-			if (i !=8)
+		if (n1 != n2 && n1 < n2)
+		{
+			putchar(n1 + 48);
+			putchar(n2 + 48);
+
+			if (n1 + n2 != 17)
 			{
 				putchar(',');
-				putchar('');
+				putchar(' ');
 			}
 		}
+		++n2;
 	}
+		++n1;
+		}
+	putchar('\n');
+	return (0);
 }
-putchar('\n');
 
-return (0);
-}
