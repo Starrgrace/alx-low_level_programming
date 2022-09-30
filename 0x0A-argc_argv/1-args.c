f23d8bce7e32ae5c72185b9504f0b641baf4f495
Starrgrace <olasunkanmisekani17@gmail.com>
2022-09-30 08:26:18 -0700

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
  * main - write a program that prints the number of arguments pased into it
- * @args: this is argument for count
+ * @argc: this is argument for count
  * @argv: this is argument for vector
  *
  * Return: return 0
  */
-int main(int args, char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("d\n", args - 1);
+	printf("%d\n", argc - 1);
 	(void)argv;
 	return (0);
 }
