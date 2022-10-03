69e6278cfd52a3b9dbe7904fe1e08770612ad92d
Starrgrace <olasunkanmisekani17@gmail.com>
2022-10-03 10:34:43 -0700

    task 0

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcat - use concatenation to join strings
+ * @dest: string that will be appended
+ * @src: source string
+ *
+ * Return: A pointer to the destination
+ */
+char *_strcat(char *dest, char *src)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _islower -to check for lowercase character
+ * @c: int c is passed into the function as argument
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
